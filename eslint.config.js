4c4c5877652dffa11fb4e48569f109fe0924ad93
Kirill T <nan1911.work@gmail.com>
2024-11-24 17:41:07 +0500

    init commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsparser from 'typescript-eslint/parser';
+
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: module,
+      parser: tsparser,
+    },
+  }
+);
