e931ac88a62761fd521fd2b2a4e1a239f86847cd
Kirill T <nan1911.work@gmail.com>
2024-11-24 21:46:01 +0500

    import command registred

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,8 +1,17 @@
-import { CLIApplication, HelpCommand, VersionCommand } from "./cli/index.js";
+import {
+  CLIApplication,
+  HelpCommand,
+  VersionCommand,
+  ImportCommand,
+} from "./cli/index.js";
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
-  cliApplication.registerCommands([new HelpCommand(), new VersionCommand()]);
+  cliApplication.registerCommands([
+    new HelpCommand(),
+    new VersionCommand(),
+    new ImportCommand(),
+  ]);
 
   cliApplication.processCommand(process.argv);
 }
