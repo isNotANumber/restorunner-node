e2fdad70eaea3c97935cbb5bc47155effa76a9d1
Kirill T <nan1911.work@gmail.com>
2024-11-24 21:42:07 +0500

    tsv file reader class added

---
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -0,0 +1,98 @@
+import { readFileSync } from "node:fs";
+
+import { FileReader } from "./file-reader.interface.js";
+import { Offer } from "../../types/offer.type.js";
+import { Location } from "../../types/location.type.js";
+import { Contacts } from "../../types/contacts.type.js";
+
+export class TSVFileReader implements FileReader {
+  private rawData = "";
+
+  constructor(private readonly filename: string) {}
+
+  private validateRawData(): void {
+    if (!this.rawData) {
+      throw new Error("File was not read");
+    }
+  }
+
+  private parseRawDataToOffers(): Offer[] {
+    return this.rawData
+      .split("\n")
+      .filter((row) => row.trim.length > 0)
+      .map((line) => this.parseLineToOffer(line));
+  }
+
+  private parseLineToOffer(line: string): Offer {
+    const [
+      id,
+      title,
+      type,
+      price,
+      latitude,
+      longitude,
+      zoom,
+      isFavorite,
+      isPopular,
+      rating,
+      description,
+      goods,
+      images,
+      phone,
+      email,
+      telegram,
+    ] = line.split("\t");
+
+    return {
+      id,
+      title,
+      type,
+      price,
+      location: this.parseLocation(latitude, longitude, zoom),
+      isFavorite: Boolean(isFavorite),
+      isPopular: Boolean(isPopular),
+      rating: parseInt(rating),
+      description,
+      goods: this.parseGoods(goods),
+      images: this.parseImages(images),
+      contacts: this.parseContacts(phone, email, telegram),
+    };
+  }
+
+  private parseLocation(
+    latitude: string,
+    longitude: string,
+    zoom: string
+  ): Location {
+    return {
+      latitude: parseInt(latitude),
+      longitude: parseInt(longitude),
+      zoom: parseInt(zoom),
+    };
+  }
+
+  private parseGoods(goodsString: string): string[] {
+    return goodsString.split(";");
+  }
+
+  private parseImages(imagesString: string): string[] {
+    return imagesString.split(";");
+  }
+
+  private parseContacts(
+    phone: string,
+    email: string,
+    telegram: string
+  ): Contacts {
+    return { phone, email, telegram };
+  }
+
+  public read(): void {
+    this.rawData = readFileSync(this.filename, { encoding: "utf8" });
+  }
+
+  public toArray(): Offer[] {
+    this.validateRawData();
+    return this.parseRawDataToOffers();
+  }
+}
