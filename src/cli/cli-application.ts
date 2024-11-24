4fea2001be5844e264245a29c7bba88b6edef431
Kirill T <nan1911.work@gmail.com>
2024-11-24 20:20:51 +0500

    cli app class extended

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,12 @@
 import { Command } from "./commands/command.interface.js";
+import { CommandParser } from "./command-parser.js";
 
 type CommandCollection = Record<string, Command>;
 
 export class CLIApplication {
   private commands: CommandCollection = {};
+
+  constructor(private readonly defaultCommand: string = "--help") {}
 
   public registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
@@ -13,5 +16,27 @@ export class CLIApplication {
 
       this.commands[command.getName()] = command;
     });
+  }
+
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  public getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(
+        `The default command (${this.defaultCommand}) is not registred.`
+      );
+    }
+
+    return this.commands[this.defaultCommand];
+  }
+
+  public parseCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
