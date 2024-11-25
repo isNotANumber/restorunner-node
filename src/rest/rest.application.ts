60c26c4f1c34857ffc2faf2e65712bec108556bf
Kirill T <nan1911.work@gmail.com>
2024-11-25 23:51:37 +0500

    rest schema updated

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,14 +1,16 @@
 import { Logger } from "../shared/libs/logger/index.js";
-import { Config } from "../shared/libs/config/index.js";
+import { Config, RestSchema } from "../shared/libs/config/index.js";
 
 export class RestApplication {
   constructor(
     private readonly logger: Logger,
-    private readonly config: Config
+    private readonly config: Config<RestSchema>
   ) {}
 
   public async init() {
     this.logger.info("Application initialization");
     this.logger.info(`Get value from env $PORT: ${this.config.get("PORT")}`);
+    this.logger.info(`Get value from env $SALT: ${this.config.get("SALT")}`);
+    this.logger.info(`Get value from env $DB_HOST: ${this.config.get("DB_HOST")}`);
   }
 }
