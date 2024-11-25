76987587d6d7bbf1f63807e9d73c02df99680edb
Kirill T <nan1911.work@gmail.com>
2024-11-26 00:21:28 +0500

    IoC and DI integrated

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,4 @@ export { Offer } from "./offer.type.js";
 export { Location } from "./location.type.js";
 export { Contacts } from "./contacts.type.js";
 export { MockServerData } from "./mock-server-data.type.js";
+export { Component } from "./component.enum.js";
