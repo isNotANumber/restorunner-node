83183ba28a638cde5a1bb4ceb5de2c600a5d6c8b
Kirill T <nan1911.work@gmail.com>
2024-11-25 15:26:56 +0500

    generate command added

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,4 +3,5 @@ export { CommandParser } from "./command-parser.js";
 
 export { HelpCommand } from "./commands/help.command.js";
 export { VersionCommand } from "./commands/version.command.js";
-export { ImportCommand } from './commands/import.command.js';
+export { ImportCommand } from "./commands/import.command.js";
+export { GenerateCommand } from "./commands/generate.command.js";
