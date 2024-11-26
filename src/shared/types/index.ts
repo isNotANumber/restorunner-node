faa597309e280b9caab2bbcb6df7666f12663fcd
NaN <nan1911.work@gmail.com>
2024-11-26 19:24:01 +0500

    user type added

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,3 +3,4 @@ export { Location } from "./location.type.js";
 export { Contacts } from "./contacts.type.js";
 export { MockServerData } from "./mock-server-data.type.js";
 export { Component } from "./component.enum.js";
+export { User } from "./user.type.js";
