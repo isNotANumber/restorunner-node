f1960fbaa550b629f23934ef7affdba68e02e1fe
NaN <nan1911.work@gmail.com>
2024-11-26 23:59:21 +0500

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import tsparser from 'typescript-eslint/parser';
-
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tsparser from "@typescript-eslint/parser";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "./eslint.config.js"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    files: ["**/*.ts"],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: module,
+      sourceType: "module",
       parser: tsparser,
     },
   }
