24d3580de1de97ade69ed98d8e07c412170e7c72
NaN <nan1911.work@gmail.com>
2024-11-27 00:58:04 +0500

    user service added

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,4 +1,5 @@
 export { UserModel, UserEntity } from "./user.entity.js";
 export { CreateUserDto } from "./dto/create-user.dto.js";
+export { UserService } from "./user-service.interface.js";
 export { DefaultUserService } from "./default-user.service.js";
 export { createUserContainer } from "./user.container.js";
