61b3450a96eec7c5c8e155dbbe2f9a82f55edf56
NaN <nan1911.work@gmail.com>
2024-11-27 22:30:59 +0500

    json middleware added

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -35,13 +35,17 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
-  private async _initServer() {
-    const port = this.config.get("PORT");
-    this.server.listen(port);
+  private async _initMiddleware() {
+    this.server.use(express.json());
   }
 
   private async _initControllers() {
     this.server.use("/categories", this.categoryController.router);
+  }
+
+  private async _initServer() {
+    const port = this.config.get("PORT");
+    this.server.listen(port);
   }
 
   public async init() {
@@ -50,6 +54,10 @@ export class RestApplication {
     this.logger.info("Init database…");
     this._initDb();
     this.logger.info("Init database completed");
+
+    this.logger.info("Init app-level middleware");
+    await this._initMiddleware();
+    this.logger.info("App-level middleware initialization completed");
 
     this.logger.info("Init controllers");
     await this._initControllers();
