3072b11c4a529abed88a58491db9e6621a10389c
NaN <nan1911.work@gmail.com>
2024-11-27 22:24:22 +0500

    fillDTO helper added

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,3 +1,5 @@
+import { ClassConstructor, plainToInstance } from "class-transformer";
+
 export function generateRandomValue(
   min: number,
   max: number,
@@ -19,4 +21,10 @@ export function getRandomItem<T>(items: T[]): T {
 
 export function getErrorMessage(error: unknown): string {
   return error instanceof Error ? error.message : "";
+}
+
+export function fillDTO<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
+  return plainToInstance(someDto, plainObject, {
+    excludeExtraneousValues: true,
+  });
 }
