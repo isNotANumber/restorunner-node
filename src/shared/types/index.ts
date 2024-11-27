543610a75423953ff1d377dd0d5c14becef4d521
NaN <nan1911.work@gmail.com>
2024-11-27 16:53:18 +0500

    category type added

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export { Contacts } from "./contacts.type.js";
 export { MockServerData } from "./mock-server-data.type.js";
 export { Component } from "./component.enum.js";
 export { User } from "./user.type.js";
+export { Category } from "./category.type.js";
