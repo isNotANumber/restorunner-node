ef1ebea6155dab51bdf439d41ce180c4a3198d36
NaN <nan1911.work@gmail.com>
2024-11-30 13:53:24 +0500

    get offers by category method added

--- src/shared/modules/category/category.controller.ts
+++ src/shared/modules/category/category.controller.ts
@@ -1,30 +1,53 @@
 import { inject, injectable } from "inversify";
 import { Request, Response } from "express";
 
-import { BaseController, HttpMethod } from "../../libs/rest/index.js";
+import {
+  BaseController,
+  HttpMethod,
+  RequestQuery,
+} from "../../libs/rest/index.js";
 import { Logger } from "../../libs/logger/index.js";
 import { Component } from "../../types/index.js";
 import { CategoryService } from "./category-service.interface.js";
 import { fillDTO } from "../../helpers/index.js";
 import { CategoryRdo } from "./rdo/category.rdo.js";
+import { OfferRdo, OfferService } from "../offer/index.js";
+import { ParamCategoryId } from "./type/param-categoryid.type.js";
 
 @injectable()
 export class CategoryController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.CategoryService)
-    private readonly categoryService: CategoryService
+    private readonly categoryService: CategoryService,
+    @inject(Component.OfferService) private readonly offerService: OfferService
   ) {
     super(logger);
 
     this.logger.info("Register routes for CategoryController…");
 
     this.addRoute({ path: "/", method: HttpMethod.Get, handler: this.index });
+    this.addRoute({
+      path: "/:categoryId/offers",
+      method: HttpMethod.Get,
+      handler: this.getOffersFromCategory,
+    });
   }
 
   public async index(_req: Request, res: Response): Promise<void> {
     const categories = await this.categoryService.find();
     const responseData = fillDTO(CategoryRdo, categories);
     this.ok(res, responseData);
+  }
+
+  public async getOffersFromCategory(
+    { params, query }: Request<ParamCategoryId, unknown, unknown, RequestQuery>,
+    res: Response
+  ): Promise<void> {
+    const offers = await this.offerService.findByCategoryId(
+      params.categoryId,
+      query.limit
+    );
+    this.ok(res, fillDTO(OfferRdo, offers));
   }
 }
---
+++ src/shared/modules/category/type/param-categoryid.type.ts
@@ -0,0 +1,7 @@
+import { ParamsDictionary } from "express-serve-static-core";
+
+export type ParamCategoryId =
+  | {
+      categoryId: string;
+    }
+  | ParamsDictionary;
