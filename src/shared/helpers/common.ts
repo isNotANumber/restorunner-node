87392dca2d604c9de3615695005316166b7d9e6f
NaN <nan1911.work@gmail.com>
2024-12-02 17:45:24 +0500

    error validation reducer added

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,4 +1,6 @@
 import { ClassConstructor, plainToInstance } from "class-transformer";
+import { ValidationError } from "class-validator";
+import { ValidationErrorField } from "../libs/rest/index.js";
 
 export function generateRandomValue(
   min: number,
@@ -33,4 +35,14 @@ export function createErrorObject(message: string) {
   return {
     error: message,
   };
+}
+
+export function reduceValidationErrors(
+  errors: ValidationError[]
+): ValidationErrorField[] {
+  return errors.map(({ property, value, constraints }) => ({
+    property,
+    value,
+    messages: constraints ? Object.values(constraints) : [],
+  }));
 }
