4234c55f6eb774330e905242a70bcf6d20d237d4
NaN <nan1911.work@gmail.com>
2024-12-02 14:39:07 +0500

    uloads dir created

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -11,6 +11,7 @@ export type RestSchema = {
   DB_PASSWORD: string;
   DB_PORT: string;
   DB_NAME: string;
+  UPLOAD_DIRECTORY: string;
   JWT_SECRET: string;
 };
 
@@ -61,6 +62,12 @@ export const configRestSchema = convict<RestSchema>({
     doc: "Secret for sign JWT",
     format: String,
     env: "JWT_SECRET",
+    default: null,
+  },
+  UPLOAD_DIRECTORY: {
+    doc: "Directory for upload files",
+    format: String,
+    env: "UPLOAD_DIRECTORY",
     default: null,
   },
 });
