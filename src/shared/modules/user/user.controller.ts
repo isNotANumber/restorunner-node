db491c91d9417dbac1af0a7acb5fe50df6f4b8ea
NaN <nan1911.work@gmail.com>
2024-12-02 14:59:23 +0500

    login implemented

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -20,13 +20,17 @@ import { UserRdo } from "./rdo/user.rdo.js";
 import { LoginUserRequest } from "./login-user-request.type.js";
 import { CreateUserDto } from "./dto/create-user.dto.js";
 import { LoginUserDto } from "./dto/login-user.dto.js";
+import { AuthService } from "../auth/index.js";
+import { LoggedUserRdo } from "./rdo/logged-user.rdo.js";
 
 @injectable()
 export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.UserService) private readonly userService: UserService,
-    @inject(Component.Config) private readonly configService: Config<RestSchema>
+    @inject(Component.Config)
+    private readonly configService: Config<RestSchema>,
+    @inject(Component.AuthService) private readonly authService: AuthService
   ) {
     super(logger);
     this.logger.info("Register routes for UserController…");
@@ -78,25 +82,14 @@ export class UserController extends BaseController {
     this.created(res, fillDTO(UserRdo, result));
   }
 
-  public async login(
-    { body }: LoginUserRequest,
-    _res: Response
-  ): Promise<void> {
-    const existsUser = await this.userService.findByEmail(body.email);
-
-    if (!existsUser) {
-      throw new HttpError(
-        StatusCodes.UNAUTHORIZED,
-        `User with email ${body.email} not found.`,
-        "UserController"
-      );
-    }
-
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      "Not implemented",
-      "UserController"
-    );
+  public async login({ body }: LoginUserRequest, res: Response): Promise<void> {
+    const user = await this.authService.verify(body);
+    const token = await this.authService.authenticate(user);
+    const responseData = fillDTO(LoggedUserRdo, {
+      email: user.email,
+      token,
+    });
+    this.ok(res, responseData);
   }
 
   public async uploadAvatar(req: Request, res: Response) {
