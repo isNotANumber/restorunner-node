c91ef70581b5a5922b3ae0854b3199795648fc89
NaN <nan1911.work@gmail.com>
2024-12-02 14:24:52 +0500

    documents exists middleware created

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,3 +5,4 @@ export { MockServerData } from "./mock-server-data.type.js";
 export { Component } from "./component.enum.js";
 export { User } from "./user.type.js";
 export { Category } from "./category.type.js";
+export { DocumentExists } from "./document-exists.interface.js";
