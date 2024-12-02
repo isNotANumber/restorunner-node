a51636ed4dd2a234d1ee6d4d94866deb10fbfada
NaN <nan1911.work@gmail.com>
2024-12-02 14:48:22 +0500

    uploadAvatar method created

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { inject, injectable } from "inversify";
-import { Response } from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 import {
   BaseController,
   HttpError,
+  UploadFileMiddleware,
   ValidateDtoMiddleware,
+  ValidateObjectIdMiddleware,
   HttpMethod,
 } from "../../libs/rest/index.js";
 import { Logger } from "../../libs/logger/index.js";
@@ -40,6 +42,18 @@ export class UserController extends BaseController {
       method: HttpMethod.Post,
       handler: this.login,
       middlewares: [new ValidateDtoMiddleware(LoginUserDto)],
+    });
+    this.addRoute({
+      path: "/:userId/avatar",
+      method: HttpMethod.Post,
+      handler: this.uploadAvatar,
+      middlewares: [
+        new ValidateObjectIdMiddleware("userId"),
+        new UploadFileMiddleware(
+          this.configService.get("UPLOAD_DIRECTORY"),
+          "avatar"
+        ),
+      ],
     });
   }
 
@@ -83,5 +97,11 @@ export class UserController extends BaseController {
       "Not implemented",
       "UserController"
     );
+  }
+
+  public async uploadAvatar(req: Request, res: Response) {
+    this.created(res, {
+      filepath: req.file?.path,
+    });
   }
 }
