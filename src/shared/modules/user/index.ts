d37825569cb295ee67454de23c2733c5de337b5b
NaN <nan1911.work@gmail.com>
2024-12-02 13:37:44 +0500

    loginUserDto added

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,6 +1,7 @@
 export { UserModel, UserEntity } from "./user.entity.js";
 export { CreateUserDto } from "./dto/create-user.dto.js";
 export { UpdateUserDto } from "./dto/update-user.dto.js";
+export { LoginUserDto } from "./dto/login-user.dto.js";
 export { UserService } from "./user-service.interface.js";
 export { DefaultUserService } from "./default-user.service.js";
 export { createUserContainer } from "./user.container.js";
