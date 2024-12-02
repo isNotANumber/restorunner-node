a3080e4f29dfd78d800b0449892f61e12d7c034f
NaN <nan1911.work@gmail.com>
2024-12-02 15:43:20 +0500

    parse token middleware added

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -8,6 +8,7 @@ import { Component } from "../shared/types/index.js";
 import { DatabaseClient } from "../shared/libs/database-client/index.js";
 import { getMongoURI } from "../shared/helpers/index.js";
 import { Controller, ExceptionFilter } from "../shared/libs/rest/index.js";
+import { ParseTokenMiddleware } from "../shared/libs/rest/middleware/parse-token.middleware.js";
 
 @injectable()
 export class RestApplication {
@@ -45,12 +46,19 @@ export class RestApplication {
   }
 
   private async _initMiddleware() {
+    const authenticateMiddleware = new ParseTokenMiddleware(
+      this.config.get("JWT_SECRET")
+    );
+
     this.server.use(express.json());
     this.server.use(
       "/upload",
       express.static(this.config.get("UPLOAD_DIRECTORY"))
     );
     this.server.use(cors());
+    this.server.use(
+      authenticateMiddleware.execute.bind(authenticateMiddleware)
+    );
   }
 
   private async _initControllers() {
